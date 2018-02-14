22c188704cb09aae754598436ea1fb05469279ee
Aleksandyr <aleksandyr.kamenov@gmail.com>
2018-02-14 15:58:34 +0200

    add dropdown initial impl

---
+++ src/drop-down/drop-down.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, NgModule } from "@angular/core";
+import { IgxInputModule,  IgxListModule, IgxToggleModule } from "../main";
+@Component({
+    selector: "igx-drop-down",
+    templateUrl: "./drop-down.component.html"
+})
+export class IgxDropDownComponent {
+    @Input()
+    public data = [];
+
+    @Input()
+    public textKey: string;
+
+    @Input()
+    public valueKey: string;
+}
+
+@NgModule({
+    declarations: [IgxDropDownComponent],
+    exports: [IgxDropDownComponent],
+    imports: [IgxToggleModule, IgxInputModule, IgxListModule]
+})
+export class IgxDropDownModule {}
--- src/main.ts
+++ src/main.ts
@@ -47,3 +47,4 @@ export * from "./slider/slider.component";
 export * from "./scroll/scroll.component";
 export * from "./date-picker/date-picker.component";
 export * from "./directives/toggle/toggle.directive";
+export * from "./drop-down/drop-down.component";
